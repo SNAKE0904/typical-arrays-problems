36b875e37efbc83a8fc4452c9cfee69d0697f524
Vanya Kirdun <55948647+SNAKE0904@users.noreply.github.com>
2020-10-01 18:48:31 +0300

    Update index.js
    init: complet task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || !array.length || array == null){
+        return 0;
+    } else{
+        let min = array[0];
+        for(let x = 1; x < array.length; x++){
+            if(array[x] < min){
+                min = array[x];
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || !array.length || array == null){
+        return 0;
+    } else{
+        let max = array[0];
+        for(let x = 1; x < array.length; x++){
+            if(array[x] > max){
+                max = array[x];
+            }
+        }
+        return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || !array.length || array == null){
+        return 0;
+    } else {
+        let avg = 0;
+        for(let x = 0; x < array.length; x++){
+            avg += array[x];
+        }
+        return avg / array.length;
+    }
 }
